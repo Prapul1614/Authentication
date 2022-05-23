3d816745a803451e0b38109088dd3839a56dba51
SAI PRAPUL REDDY POOLA <yourEmail>
2022-05-23 23:55:44 +0530

    hash bcrypt along with salt rounds

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,30 +40,35 @@ app.get("/register", function(req, res){
 
 // if already registested say to them
 app.post("/register", function( req ,res ){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
+    
+    bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
+    
 })
 
 // if password is incorrect say so
 // if email not found redirect them
 app.post("/login", function( req , res ){
     const email = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     
     User.findOne({email: email}, function(err , foundUser){
         if(err) console.log(err);
         else{
-            if(foundUser.password === password) 
-              res.render("secrets");
+            bcrypt.compare(password, foundUser.password).then(function(result) {
+                if(result === true) res.render("secrets");
+            });
         }
     })
 });
