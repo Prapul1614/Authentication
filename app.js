65e233f2647eca573705c50b4788e60cf6e1f2af
SAI PRAPUL REDDY POOLA <yourEmail>
2022-05-23 23:33:30 +0530

    hash using md5

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
+// see git log to see different versions
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -21,8 +22,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -42,7 +41,7 @@ app.get("/register", function(req, res){
 app.post("/register", function( req ,res ){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
@@ -57,7 +56,7 @@ app.post("/register", function( req ,res ){
 // if email not found redirect them
 app.post("/login", function( req , res ){
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     
     User.findOne({email: email}, function(err , foundUser){
         if(err) console.log(err);
